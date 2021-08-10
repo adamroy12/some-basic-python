3b53ac41e42c76e44f9aca02d77cf2c4f99cb302
Adam Royal <adamroyal12@gmail.com>
2021-08-10 13:23:03 +0100

    changes to weather.py

--- temp.py
+++
@@ -1,7 +0,0 @@
-temperature = 75
-
-if temperature > 80 or temperature <60:
-    print("Stay inside!")
-else:
-    print("Enjoy the outdoors!")
-print("Have a good day!")
---
+++ weather.py
@@ -0,0 +1,7 @@
+temperature = 75
+forecast = "sunny"
+
+if temperature < 80 and forecast != "rain":
+    print("Go outside!")
+else:
+    print("Stay inside!")
