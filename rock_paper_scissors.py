15c3fa8b7173379ce6bff49705b2e2acae5b77b4
Adam Royal <adamroyal12@gmail.com>
2021-08-10 13:53:35 +0100

    testing randomness in Rock paper scissors

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,4 +1,15 @@
-computer_choice = "paper"
+import random
+
+computer_choice = random.randint(1,3)
+
+if computer_choice == 1:
+    computer_choice = "rock"
+elif computer_choice == 2:
+    computer_choice = "paper"
+elif computer_choice == 3:
+    computer_choice = "scissors"
+
+print(computer_choice)
 
 user_choice = input("rock, paper, or scissors?\n")
 
