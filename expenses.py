3097e04d42fb6ea5337a7a3096f8ba163cea5a11
Adam Royal <adamroyal12@gmail.com>
2021-08-10 15:23:48 +0100

    expenses sum refactored using sum function

--- expenses.py
+++ expenses.py
@@ -1,7 +1,4 @@
 expenses = [10.50, 8, 5, 15, 20, 5 ,3]
-sum = 0
+total = sum(expenses)
 
-for element in expenses:
-    sum = sum + element
-
-print("You spent $", sum, " on lunch this week.", sep="")
+print("You spent $", total, " on lunch this week.", sep="")
