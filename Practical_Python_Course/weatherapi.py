1c0c78bb9900fd8975f9ad8e77680fe84833ddcc
Adam Royal <adamroyal12@gmail.com>
2021-09-29 16:31:01 +0100

    refactored and added functions to weatherapi.py

--- Practical_Python_Course/weatherapi.py
+++ Practical_Python_Course/weatherapi.py
@@ -1,18 +1,29 @@
 import requests
 
 # api key can be found on openweathermap.org
-api_key = ""
-city = ""
-url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
 
-request = requests.get(url)
-json = request.json()
+def get_weather_desc_and_temp():
+    api_key = ""
+    city = ""
+    url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key+"&units=metric"
 
-description = json.get("weather")[0].get("description")
-print("Today's forcast is "+description)
+    request = requests.get(url)
+    json = request.json()
 
-temp_min = json.get("main").get("temp_min")
-print("with low temperatures of", temp_min)
+    description = json.get("weather")[0].get("description")
+    temp_max = json.get("main").get("temp_max")
+    temp_min = json.get("main").get("temp_min")
 
-temp_max = json.get("main").get("temp_max")
-print("and high temperatures of", temp_max)
+    return {'description': description,
+            'temp_min': temp_min,
+            'temp_max': temp_max
+            }
+
+def main():
+    weather_dict = get_weather_desc_and_temp()
+
+    print("with low temperatures of", weather_dict.get('temp_min'))
+    print("and high temperatures of", weather_dict.get('temp_max'))
+    print("Today's forcast is "+ weather_dict.get('description'))
+
+main()
